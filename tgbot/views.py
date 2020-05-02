b1fd9dbfec1720e1f642c545424c08b9421ba0a8
Whom <m0rtydisg@gmail.com>
2020-05-03 01:16:14 +0300

    commit

--- tgbot/views.py
+++ tgbot/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, HttpResponse
 from django.conf import settings
+from django.views.decorators.csrf import csrf_exempt
 
 import requests
 
@@ -10,6 +11,7 @@ chat_id = '415924423'
 text = 'Ваш статус заказа изменен!'
 
 
+@csrf_exempt
 def notice_status(request):
     if request.method == 'POST':
         response = requests.get(base_url + settings.TOKEN + '/sendMessage?chat_id=' + str(chat_id) + '&text=' + text)
